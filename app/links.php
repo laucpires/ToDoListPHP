1af39b0157b6a2829db898ac9246b2daa342fb39
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-11-27 17:05:49 -0300

    Create links.php

---
+++ app/links.php
@@ -0,0 +1,8 @@
+<?php
+$css = "../public/assets/css/style.css";
+$js = "../public/assets/js/script.js";
+?>
+<link rel="stylesheet" href="<?php echo $css; ?>">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="<?php echo $js ?>" defer></script>
