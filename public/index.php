f826e6b54aed6e1c862f8be7cef8b6ae68b58cb1
Laura Cargnelutti Pires <laucpires5@gmail.com>
2023-11-28 10:33:09 -0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php 
+    session_start();
+    include "../includes/links.php"; 
+    ?>
+    <link rel="shortcut icon" href="../public/assets/img/lista-de-controle.png" type="image/x-icon">
+    <title>To Do PHP</title>
+</head>
+
+<body>
+    <div class="container-header">
+        <header>
+            <h1>To Do List</h1>
+        </header>
+        <?php include "../app/AddTask.php"; ?>
+    </div>
+</body>
+
+</html>
